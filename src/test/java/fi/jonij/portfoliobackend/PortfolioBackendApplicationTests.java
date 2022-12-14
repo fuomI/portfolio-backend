6b1bc7f278e1338a11132ad64aa53153ad931c80
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-14 11:37:06 +0200

    Project task #4 - Added main sourcefiles and test sourcefiles

---
+++ src/test/java/fi/jonij/portfoliobackend/PortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.jonij.portfoliobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
