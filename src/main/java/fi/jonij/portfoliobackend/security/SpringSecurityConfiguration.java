e2da782643dd4da2d60065c4af5c908982347495
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-14 14:31:13 +0200

    Coding task #1.2 - Created method for creating UserDetails

--- src/main/java/fi/jonij/portfoliobackend/security/SpringSecurityConfiguration.java
+++ src/main/java/fi/jonij/portfoliobackend/security/SpringSecurityConfiguration.java
@@ -2,12 +2,31 @@ package fi.jonij.portfoliobackend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.function.Function;
+
 @Configuration
 public class SpringSecurityConfiguration {
-    
+
+    // Method for creating user with bcrypt encoder and two roles: "USER" & "ADMIN"
+    private UserDetails createNewUser(String username, String password) {
+        Function<String, String> passwordEncoder =
+                input -> passwordEncoder().encode(input);
+
+        UserDetails userDetails = User.builder()
+                .passwordEncoder(passwordEncoder)
+                .username(username)
+                .password(password)
+                .roles("USER", "ADMIN")
+                .build();
+
+        return userDetails;
+    }
+
     // Method for creating bcrypt password encoder with default strength 10
     @Bean
     public PasswordEncoder passwordEncoder() {
