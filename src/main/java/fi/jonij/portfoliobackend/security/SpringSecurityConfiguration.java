f28660a9d0a37bece0d7471a454d038dc5683e61
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-14 14:20:45 +0200

    Coding task #1.2 - Created method for creating bcrypt password encoder

--- src/main/java/fi/jonij/portfoliobackend/security/SpringSecurityConfiguration.java
+++ src/main/java/fi/jonij/portfoliobackend/security/SpringSecurityConfiguration.java
@@ -1,7 +1,16 @@
 package fi.jonij.portfoliobackend.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SpringSecurityConfiguration {
+    
+    // Method for creating bcrypt password encoder with default strength 10
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
