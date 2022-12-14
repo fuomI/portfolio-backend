bf56f0f39a69798417ac1e52d2ea0b5102192058
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-14 17:23:12 +0200

    Coding task #1.3 - Added method getLoggedInUsername() to get the name of the authenticated user

--- src/main/java/fi/jonij/portfoliobackend/login/WelcomeController.java
+++ src/main/java/fi/jonij/portfoliobackend/login/WelcomeController.java
@@ -1,10 +1,20 @@
 package fi.jonij.portfoliobackend.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class WelcomeController {
 
 
+
+    // Returns the username of authenticated user
+    private String getLoggedInUsername() {
+        Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+
+        return authentication.getName();
+    }
 }
 
