9539200902611cc1bc6d8c81c967b78cbc0cfae3
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-15 11:20:02 +0200

    Coding task 2: Added all fields to Project class, marked Project as an @Entity, marked field 'id' as the primary key (@Id annotation) and set it to be @GeneratedValue

--- src/main/java/fi/jonij/portfoliobackend/project/Project.java
+++ src/main/java/fi/jonij/portfoliobackend/project/Project.java
@@ -1,4 +1,24 @@
 package fi.jonij.portfoliobackend.project;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String username;
+    private String projectName;
+    private String projectType;
+    private LocalDate dateOfCompletion;
+    private boolean isDeployed;
+    private String sourceCodeURL;
+    private String projectURL;
+    private String projectImageFilename;
+
 }
