6e63af0996250b72bc84d766a61555aa4e2528e5
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-15 13:10:53 +0200

    Coding task 2.4: Test data set up

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO PROJECT
+(ID, USERNAME, PROJECT_NAME, PROJECT_TYPE, DATE_OF_COMPLETION,
+ IS_DEPLOYED, SOURCE_CODE_URL, PROJECT_URL, PROJECT_IMAGE_FILENAME)
+VALUES
+    (1001, 'tester', 'test project one', 'Coding', CURRENT_DATE(),
+     true, 'http://www.github.com/testproj1', 'http://railway.app/testproj1',
+     'testproj1.jpg');
+
+INSERT INTO PROJECT
+(ID, USERNAME, PROJECT_NAME, PROJECT_TYPE, DATE_OF_COMPLETION,
+ IS_DEPLOYED)
+VALUES
+    (1002, 'tester', 'test project two', 'UX', CURRENT_DATE(),
+     false);
+
