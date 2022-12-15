54ab3790a5db3bf313bd3ea64ca997428b0bfa1b
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-15 12:01:21 +0200

    Coding task 2: Added validation for Project entity's fields

--- src/main/java/fi/jonij/portfoliobackend/project/Project.java
+++ src/main/java/fi/jonij/portfoliobackend/project/Project.java
@@ -3,6 +3,7 @@ package fi.jonij.portfoliobackend.project;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
@@ -11,14 +12,27 @@ public class Project {
 
     @Id
     @GeneratedValue
+    @NotEmpty
     private int id;
+    @NotEmpty
     private String username;
+    @NotEmpty
+    @Size(min=2, message="Enter at least 2 characters")
     private String projectName;
+    @NotEmpty
+    @Size(min=2, message="Enter at least 2 characters")
+    @Pattern(regexp="^[a-zA-z]+$", message="Enter only letters")
     private String projectType;
+    @NotEmpty
+    @Past
     private LocalDate dateOfCompletion;
+    @NotNull
     private boolean isDeployed;
+    @Pattern(regexp="((http|https)://)?")
     private String sourceCodeURL;
+    @Pattern(regexp="((http|https)://)?")
     private String projectURL;
+    @Pattern(regexp="(.jpg|.jpeg|.png)$")
     private String projectImageFilename;
 
 }
