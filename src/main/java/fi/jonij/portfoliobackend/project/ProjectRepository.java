6c82bf6eb001844853e206669ccfeacce818f5ff
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-15 14:14:45 +0200

    Coding task 2.1: Added findByUsername() Method to ProjectRepository interface

--- src/main/java/fi/jonij/portfoliobackend/project/ProjectRepository.java
+++ src/main/java/fi/jonij/portfoliobackend/project/ProjectRepository.java
@@ -2,5 +2,9 @@ package fi.jonij.portfoliobackend.project;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
+
+    List<Project> findByUsername(String username);
 }
