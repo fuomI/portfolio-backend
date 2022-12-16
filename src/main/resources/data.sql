af68ade7174375ed915d844e986ca65bd3b6c60a
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-16 10:43:43 +0200

    Coding task 3.2: data.sql: Removed second test entry, it created an error - earlier commit had an issue

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,7 @@
 INSERT INTO PROJECT
 (ID, USERNAME, PROJECT_NAME, PROJECT_TYPE, DATE_OF_COMPLETION,
- IS_DEPLOYED, SOURCE_CODE_URL, PROJECT_URL, PROJECT_IMAGE_FILENAME)
+ DEPLOYED, SOURCE_CODE_URL, PROJECT_URL, PROJECT_IMAGE_FILENAME)
 VALUES
     (1001, 'tester', 'test project one', 'Coding', CURRENT_DATE(),
-     true, 'http://www.github.com/testproj1', 'http://railway.app/testproj1',
+     true, 'https://www.github.com/testproj1', 'https://railway.app/testproj1',
      'default.png');
-
