2f7e778ee65fe01df4a8d0cfa689de0dd58d15f3
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-16 10:42:29 +0200

    Coding task 3.2: data.sql: Removed second test entry, it created an error

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,10 +6,3 @@ VALUES
      true, 'http://www.github.com/testproj1', 'http://railway.app/testproj1',
      'default.png');
 
-INSERT INTO PROJECT
-(ID, USERNAME, PROJECT_NAME, PROJECT_TYPE, DATE_OF_COMPLETION,
- IS_DEPLOYED)
-VALUES
-    (1002, 'tester', 'test project two', 'UX', CURRENT_DATE(),
-     false);
-
