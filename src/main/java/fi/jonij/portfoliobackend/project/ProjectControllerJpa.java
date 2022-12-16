12d8261d3221d09f5038a5bd28e51ce1229b521f
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-16 09:34:29 +0200

    Coding task 3: Added ProjectRepository as variable to ProjectControllerJpa and created constructor dependency injection for that

--- src/main/java/fi/jonij/portfoliobackend/project/ProjectControllerJpa.java
+++ src/main/java/fi/jonij/portfoliobackend/project/ProjectControllerJpa.java
@@ -1,4 +1,16 @@
 package fi.jonij.portfoliobackend.project;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("name")
 public class ProjectControllerJpa {
+
+    private ProjectRepository projectRepository;
+
+    public ProjectControllerJpa(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+    
 }
