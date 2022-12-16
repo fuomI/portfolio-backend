04026ac80c17f4ab5b6083af7c5402d8745df764
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-16 15:01:38 +0200

    Coding task #4: ProjectControllerJpa: Added POST method handling for route 'add-project'

--- src/main/java/fi/jonij/portfoliobackend/project/ProjectControllerJpa.java
+++ src/main/java/fi/jonij/portfoliobackend/project/ProjectControllerJpa.java
@@ -1,9 +1,11 @@
 package fi.jonij.portfoliobackend.project;
 
+import jakarta.validation.Valid;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -31,15 +33,30 @@ public class ProjectControllerJpa {
         return "listProjects";
     }
 
-    // Route for adding a Project
+    // Route for showing the 'Add Project' -page
     @RequestMapping(value="add-project", method = RequestMethod.GET)
     public String showNewProjectPage(ModelMap model) {
         String username = getLoggedInUserName();
         Project project = new Project(username, "", "", LocalDate.now(),
-                                        true, "", "", "default.png" );
+                                        true, "", "", "" );
         model.addAttribute("project", project);
 
         return "project";
+    }
+
+    // Handling the form POST at 'add-project' route
+    @RequestMapping(value="add-project", method = RequestMethod.POST)
+    public String addNewProject(@Valid Project newProject, BindingResult result) {
+        if(result.hasErrors()) {
+            return "project";
+        }
+
+        String username = getLoggedInUserName();
+        newProject.setUsername(username);
+        newProject.setProjectImageFilename("default.png"); // Temporary hard code, until file upload implemented
+        projectRepository.save(newProject);
+
+        return "redirect:list-projects";
     }
 
     private static String getLoggedInUserName() {
