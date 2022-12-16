84af8fd0b9718b1acb8052478811226c1bd51bcd
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-16 09:45:19 +0200

    Coding task 3.1: ProjectControllerJpa: Added route for listing all projects of the logged user

--- src/main/java/fi/jonij/portfoliobackend/project/ProjectControllerJpa.java
+++ src/main/java/fi/jonij/portfoliobackend/project/ProjectControllerJpa.java
@@ -3,7 +3,11 @@ package fi.jonij.portfoliobackend.project;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.List;
 
 @Controller
 @SessionAttributes("name")
@@ -13,6 +17,16 @@ public class ProjectControllerJpa {
 
     public ProjectControllerJpa(ProjectRepository projectRepository) {
         this.projectRepository = projectRepository;
+    }
+
+    // Route for listing all projects of the logged user
+    @RequestMapping("list-projects")
+    public String listAllProjects(ModelMap model) {
+        String username = getLoggedInUserName();
+        List<Project> projects = projectRepository.findByUsername(username);
+        model.addAttribute("projects", projects);
+
+        return "listProjects";
     }
 
     private static String getLoggedInUserName() {
