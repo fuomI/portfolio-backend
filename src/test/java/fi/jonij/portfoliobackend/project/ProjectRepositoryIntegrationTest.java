03db76433bec054c2ab3800c60d361db3da4dea9
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-17 11:42:41 +0200

    Coding task 5: ProjectRepositoryIntegrationTest.java: Restored the ProjectRepositoryIntegrationTest. The reasons for tests failing were with faulty regular expression validations in Project class fields sourceCodeUrl and projectUrl. So the tests seem to be working as intended.

---
+++ src/test/java/fi/jonij/portfoliobackend/project/ProjectRepositoryIntegrationTest.java
@@ -0,0 +1,51 @@
+package fi.jonij.portfoliobackend.project;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class ProjectRepositoryIntegrationTest {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    private Project testProject = new Project("user", "testProject", "Coding",
+            "This is testProject for testing the project repository",
+            LocalDate.now(), "http://github.com", "https://railway.app/project",
+            "testproject.jpg");
+
+    Project testProject2 = new Project("tester", "testProject", "Coding",
+            "This is testProject for testing the project repository",
+            LocalDate.now(), "http://github.com",
+            "https://railway.app/project", "testproject.jpg");
+
+    @BeforeEach
+    public void saveProjectToProjectRepository () {
+        projectRepository.save(testProject);
+    }
+
+    @Test
+    public void findSavedProjectByUsername_basicScenario() {
+        Project expected = testProject;
+
+        assertEquals(expected, projectRepository.findByUsername("user").get(0));
+    }
+
+    @Test
+    public void findSavedProjectByUsername_negativeScenario() {
+        assertNotEquals(testProject2, projectRepository.findByUsername("user").get(0));
+    }
+
+    @Test
+    public void deleteProjectFromProjectRepository_basicScenario() {
+        projectRepository.delete(testProject);
+
+        assertTrue(projectRepository.findByUsername("user").isEmpty());
+    }
+}
