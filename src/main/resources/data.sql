78fc923b4f9861919b1a570302c995573f76eb5e
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-17 10:45:45 +0200

    Coding task 5: data.sql: Fixed the id of the second test project

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,7 +11,7 @@ INSERT INTO PROJECT
 (ID, USERNAME, PROJECT_NAME, PROJECT_TYPE, PROJECT_DESCRIPTION,
  DATE_OF_COMPLETION, SOURCE_CODE_URL, PROJECT_URL, PROJECT_IMAGE_FILENAME)
 VALUES
-    (1001, 'tester', 'test project two', 'UX',
+    (1002, 'tester', 'test project two', 'UX',
      'Second project for testing the backend of my portfolio',
      CURRENT_DATE(), 'https://www.github.com/testproj2',
      'https://railway.app/testproj2', 'default.png');
