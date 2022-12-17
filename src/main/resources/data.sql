96e51b17c00b429b482c57988ced6cd4edab726d
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-17 10:40:31 +0200

    Coding task 5: data.sql: Added second test project to see how multiple projects line up in the view

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,12 @@ VALUES
      'Project for testing the backend of my portfolio',
      CURRENT_DATE(), 'https://www.github.com/testproj1',
      'https://railway.app/testproj1', 'default.png');
+
+INSERT INTO PROJECT
+(ID, USERNAME, PROJECT_NAME, PROJECT_TYPE, PROJECT_DESCRIPTION,
+ DATE_OF_COMPLETION, SOURCE_CODE_URL, PROJECT_URL, PROJECT_IMAGE_FILENAME)
+VALUES
+    (1001, 'tester', 'test project two', 'UX',
+     'Second project for testing the backend of my portfolio',
+     CURRENT_DATE(), 'https://www.github.com/testproj2',
+     'https://railway.app/testproj2', 'default.png');
