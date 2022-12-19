3195b853ac0adbe2699e489758bf8f39207f4a4d
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-19 19:58:05 +0200

    Coding task 8: ProjectControllerJpaTest.java: Added annotations, MockBeans ProjectRepository & ProjectControllerJpa, added MockMvc, ALL_PROJECTS_URL variable and first test listAllProjects_basicScenario()

--- src/test/java/fi/jonij/portfoliobackend/project/ProjectControllerJpaTest.java
+++ src/test/java/fi/jonij/portfoliobackend/project/ProjectControllerJpaTest.java
@@ -1,6 +1,39 @@
 package fi.jonij.portfoliobackend.project;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@WebMvcTest(controllers = ProjectControllerJpa.class)
+@AutoConfigureMockMvc(addFilters = false) // disable Spring Security for Unit tests
 class ProjectControllerJpaTest {
 
+    @MockBean
+    private ProjectRepository projectRepository;
+
+    @MockBean
+    private ProjectControllerJpa projectControllerJpa;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    public static final String ALL_PROJECTS_URL = "http://localhost:8080/list-projects";
+
+    @Test
+    void listAllProjects_basicScenario() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(ALL_PROJECTS_URL);
+
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+
+        assertEquals(200, mvcResult.getResponse().getStatus());
+    }
 
 }
