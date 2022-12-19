448806b1c25adc1da3dcf85a3fc9afdf92ae50ad
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-19 16:57:16 +0200

    Coding task 8: WelcomeControllerWebMvcIntegrationTest.java: Added @ContextConfiguration to 'test-context.xml'

--- src/test/java/fi/jonij/portfoliobackend/login/WelcomeControllerWebMvcIntegrationTest.java
+++ src/test/java/fi/jonij/portfoliobackend/login/WelcomeControllerWebMvcIntegrationTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -13,6 +14,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @WebMvcTest(controllers = WelcomeController.class)
+@ContextConfiguration(locations= "/test-context.xml")
 class WelcomeControllerWebMvcIntegrationTest {
 
     @Autowired
