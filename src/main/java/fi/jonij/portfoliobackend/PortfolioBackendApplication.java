4e324da08a51b267762865a413e69c187f69fcba
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-19 13:00:42 +0200

    Coding task 7: PortfolioBackendApplication.java: Added the code needed for file upload.

--- src/main/java/fi/jonij/portfoliobackend/PortfolioBackendApplication.java
+++ src/main/java/fi/jonij/portfoliobackend/PortfolioBackendApplication.java
@@ -1,13 +1,27 @@
 package fi.jonij.portfoliobackend;
 
+import fi.jonij.portfoliobackend.storage.StorageProperties;
+import fi.jonij.portfoliobackend.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class PortfolioBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioBackendApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
