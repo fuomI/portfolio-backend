a95673729498c088945730e0f35568b3991cb0a7
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-19 19:47:15 +0200

    Coding task 8: PortfolioBackendApplication.java: Removed unnecessary code related to StorageService, it caused problems with testing.

--- src/main/java/fi/jonij/portfoliobackend/PortfolioBackendApplication.java
+++ src/main/java/fi/jonij/portfoliobackend/PortfolioBackendApplication.java
@@ -1,12 +1,9 @@
 package fi.jonij.portfoliobackend;
 
 import fi.jonij.portfoliobackend.storage.StorageProperties;
-import fi.jonij.portfoliobackend.storage.StorageService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -14,13 +11,6 @@ public class PortfolioBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioBackendApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner init(StorageService storageService) {
-		return (args) -> {
-			storageService.init();
-		};
 	}
 
 }
