8f9d9ba46c171dbb4776043862c9de05757343d4
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-20 15:48:27 +0200

    Coding task 9: ProjectResource: Created retrieveAllProjects() with route '/rest/projects'

--- src/main/java/fi/jonij/portfoliobackend/project/ProjectResource.java
+++ src/main/java/fi/jonij/portfoliobackend/project/ProjectResource.java
@@ -1,6 +1,11 @@
 package fi.jonij.portfoliobackend.project;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 public class ProjectResource {
@@ -11,5 +16,14 @@ public class ProjectResource {
         this.projectRepository = projectRepository;
     }
 
-    
+    @RequestMapping("/rest/projects")
+    public List<Project> retrieveAllProjects() {
+        List<Project> projects = projectRepository.findAll();
+
+        if(projects.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        return projects;
+    }
 }
