1b3f2b8a5c1d1493d2da6cc7c57efdbce6f1ac17
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-29 17:44:22 +0200

    Coding task 11: Project.java: Fixed imports to mirror new dependencies

--- src/main/java/fi/jonij/portfoliobackend/project/Project.java
+++ src/main/java/fi/jonij/portfoliobackend/project/Project.java
@@ -1,10 +1,12 @@
 package fi.jonij.portfoliobackend.project;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.*;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Entity
