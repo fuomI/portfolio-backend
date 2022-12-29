cd7bcaf6e1d70f9f6060e076f46c001478a9ed81
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-29 17:42:53 +0200

    Coding task 11: PortfolioBackendApplication: extends now SpringBootServletInitializer

--- src/main/java/fi/jonij/portfoliobackend/PortfolioBackendApplication.java
+++ src/main/java/fi/jonij/portfoliobackend/PortfolioBackendApplication.java
@@ -3,11 +3,20 @@ package fi.jonij.portfoliobackend;
 import fi.jonij.portfoliobackend.storage.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+// Commented code needed when deployed in web container
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
-public class PortfolioBackendApplication {
+public class PortfolioBackendApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PortfolioBackendApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioBackendApplication.class, args);
