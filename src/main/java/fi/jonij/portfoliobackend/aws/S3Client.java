5c10e1e5e88d3e1514a7021106d2bdf6b8aceb15
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-31 09:17:05 +0200

    Coding task 12: S3Client.java: Added S3Client class with constructor, which uses AmazonCredentials

---
+++ src/main/java/fi/jonij/portfoliobackend/aws/S3Client.java
@@ -0,0 +1,20 @@
+package fi.jonij.portfoliobackend.aws;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+public class S3Client {
+
+    private final AmazonS3 s3client;
+    private final AmazonCredentials credentials = new AmazonCredentials();
+
+    public S3Client(AmazonS3 s3client) {
+        this.s3client = AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials.getCredentials()))
+                .withRegion(Regions.EU_NORTH_1)
+                .build();
+    }
+}
