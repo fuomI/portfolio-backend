1dceff1f11aa20c3b6f38c105688132ba89bd171
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-31 10:16:05 +0200

    Coding task 12: S3BucketServiceIntegrationTest: Removed @BeforeAll and @TestInstance annotations and added s3Client as private field for the test class

--- src/test/java/fi/jonij/portfoliobackend/aws/S3BucketServiceIntegrationTest.java
+++ src/test/java/fi/jonij/portfoliobackend/aws/S3BucketServiceIntegrationTest.java
@@ -1,21 +1,17 @@
 package fi.jonij.portfoliobackend.aws;
 
 import com.amazonaws.services.s3.AmazonS3;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class S3BucketServiceIntegrationTest {
 
     @Autowired
-    S3BucketService s3BucketService;
+    private S3BucketService s3BucketService;
 
-    @BeforeAll
-    public void getS3Client() {
-        AmazonS3 s3Client = s3BucketService.getS3client();
-    }
+    private AmazonS3 s3Client = s3BucketService.getS3client();
+
+
 
 }
