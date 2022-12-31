582e01d1c7ad630e7b0c443490afd789ab504d82
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-31 10:44:36 +0200

    Coding task 12: S3BucketServiceIntegrationTest: Added initialization for s3BucketService and added test getBuckets_successfulScenario()

--- src/test/java/fi/jonij/portfoliobackend/aws/S3BucketServiceIntegrationTest.java
+++ src/test/java/fi/jonij/portfoliobackend/aws/S3BucketServiceIntegrationTest.java
@@ -1,6 +1,7 @@
 package fi.jonij.portfoliobackend.aws;
 
 import com.amazonaws.services.s3.AmazonS3;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,10 +9,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 class S3BucketServiceIntegrationTest {
 
     @Autowired
-    private S3BucketService s3BucketService;
+    private S3BucketService s3BucketService = new S3BucketService();
 
     private AmazonS3 s3Client = s3BucketService.getS3client();
 
-
+    @Test
+    public void getBuckets_successfulScenario() {
+        String expected = "jonij-portfolio-backend";
+        System.out.println(s3Client.listBuckets().toString());
+    }
 
 }
