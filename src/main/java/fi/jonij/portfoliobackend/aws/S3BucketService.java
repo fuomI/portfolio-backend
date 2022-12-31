5d9a9447031dd4c0e54ea00581f2b6cee6884fa8
Joni Jansson <joni.jansson@saunalahti.fi>
2022-12-31 09:34:37 +0200

    Coding task 12: S3BucketService: Added fields s3client and buckets and added empty constructor

--- src/main/java/fi/jonij/portfoliobackend/aws/S3BucketService.java
+++ src/main/java/fi/jonij/portfoliobackend/aws/S3BucketService.java
@@ -1,4 +1,15 @@
 package fi.jonij.portfoliobackend.aws;
 
+import com.amazonaws.services.s3.model.Bucket;
+
+import java.util.List;
+
 public class S3BucketService {
+
+    private final S3Client s3client;
+    private List<Bucket> buckets;
+
+    public S3BucketService() {
+        this.s3client = new S3Client();
+    }
 }
