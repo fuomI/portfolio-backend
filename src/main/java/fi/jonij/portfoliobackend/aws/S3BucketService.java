37580a702ae35dcf9609243e1a8f323bd321bfa7
Joni Jansson <joni.jansson@saunalahti.fi>
2023-01-03 11:38:32 +0200

    Coding task 12: S3BucketService.java: Added overloaded uploadProjectImage() method with 2 variants, one acceps MultipartFile and one accepts File

--- src/main/java/fi/jonij/portfoliobackend/aws/S3BucketService.java
+++ src/main/java/fi/jonij/portfoliobackend/aws/S3BucketService.java
@@ -2,15 +2,22 @@ package fi.jonij.portfoliobackend.aws;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.Bucket;
+import fi.jonij.portfoliobackend.storage.StorageException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class S3BucketService {
 
     private final AmazonS3 s3client;
-    private List<Bucket> buckets;
+    private final List<Bucket> buckets;
 
     public S3BucketService() {
         this.s3client = new S3Client().getS3client();
@@ -24,4 +31,50 @@ public class S3BucketService {
     public List<Bucket> getBuckets() {
         return buckets;
     }
+
+    public void uploadProjectImage(String bucketName, MultipartFile multipartFile) {
+        File file = null;
+        try {
+            if (multipartFile.isEmpty()) {
+                throw new StorageException("Failed to store empty file.");
+            }
+
+            // s3client accepts File not MultipartFile
+            file = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+
+            try (OutputStream outputStream = new FileOutputStream(file)) {
+                outputStream.write(multipartFile.getBytes());
+            }
+
+            s3client.putObject(
+                    bucketName,
+                    "project-images/" + file.getName(),
+                    file
+            );
+
+        } catch (IOException e) {
+            throw new StorageException("Failed to store file.", e);
+        } finally {
+            if (file != null) {
+                file.delete();
+            }
+        }
+    }
+    public void uploadProjectImage(String bucketName, File file) {
+        try {
+            if (file == null) {
+                throw new StorageException("Failed to store empty file.");
+            }
+
+            s3client.putObject(
+                    bucketName,
+                    "project-images/" + file.getName(),
+                    file
+            );
+
+        } catch (Exception e) {
+            throw new StorageException("Failed to store file.", e);
+        }
+    }
+    
 }
