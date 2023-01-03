09c1341d052279e39989fdac25f068f55ed30906
Joni Jansson <joni.jansson@saunalahti.fi>
2023-01-03 11:41:06 +0200

    Coding task 12: S3BucketServiceIntegrationTest: Added test uploadProjectImage_successfulScenario()

--- src/test/java/fi/jonij/portfoliobackend/aws/S3BucketServiceIntegrationTest.java
+++ src/test/java/fi/jonij/portfoliobackend/aws/S3BucketServiceIntegrationTest.java
@@ -1,11 +1,14 @@
 package fi.jonij.portfoliobackend.aws;
 
-import com.amazonaws.services.s3.AmazonS3;
+import fi.jonij.portfoliobackend.storage.StorageException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class S3BucketServiceIntegrationTest {
@@ -13,13 +16,27 @@ class S3BucketServiceIntegrationTest {
     @Autowired
     private S3BucketService s3BucketService = new S3BucketService();
 
-    private AmazonS3 s3Client = s3BucketService.getS3client();
-
     @Test
     public void getBuckets_successfulScenario() {
         String expected = "jonij-portfolio-backend";
         String actual = s3BucketService.getBuckets().get(0).getName();
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void uploadProjectImage_successfulScenario() {
+        File file = new File("src/main/resources/static/upload/default.png");
+
+        try {
+            s3BucketService.uploadProjectImage("jonij-portfolio-backend",
+                                        file);
+            System.out.println("Successful");
+
+        } catch (StorageException e) {
+            System.out.println(e.getMessage());
+            fail();
+        }
+    }
+
 
 }
