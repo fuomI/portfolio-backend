8cf76e915d1516760b7a27779a08f680675c847f
Joni Jansson <joni.jansson@saunalahti.fi>
2023-01-04 09:06:59 +0200

    Coding task 12: S3BucketServiceIntegrationTest: Added try-catch to getBuckets_successfulScenario() to catch AmazonS3Exceptions. Completed uploadProjectImage_successfulScenario() with assertEquals comparing expected key and actual key. Created getProjectImage_successfulScenario() test. Created getProjectImage_negativeScenario() test.

--- src/test/java/fi/jonij/portfoliobackend/aws/S3BucketServiceIntegrationTest.java
+++ src/test/java/fi/jonij/portfoliobackend/aws/S3BucketServiceIntegrationTest.java
@@ -1,14 +1,15 @@
 package fi.jonij.portfoliobackend.aws;
 
-import fi.jonij.portfoliobackend.storage.StorageException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
 
 import java.io.File;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class S3BucketServiceIntegrationTest {
@@ -19,20 +20,32 @@ class S3BucketServiceIntegrationTest {
     @Test
     public void getBuckets_successfulScenario() {
         String expected = "jonij-portfolio-backend";
-        String actual = s3BucketService.getBuckets().get(0).getName();
-        assertEquals(expected, actual);
+        try {
+            String actual = s3BucketService.getBuckets().get(0).getName();
+            assertEquals(expected, actual);
+
+        } catch (AmazonS3Exception e) {
+            System.out.println(e.getMessage());
+            fail();
+        }
+
     }
 
     @Test
     public void uploadProjectImage_successfulScenario() {
+        AmazonS3 s3Client = s3BucketService.getS3Client();
         File file = new File("src/main/resources/static/upload/default.png");
+        String expected = "project-images/default.png";
 
         try {
-            s3BucketService.uploadProjectImage("jonij-portfolio-backend",
-                                        file);
-            System.out.println("Successful");
+            s3BucketService.uploadProjectImage(file);
 
-        } catch (StorageException e) {
+            String actual = s3Client.getObject("jonij-portfolio-backend",
+                                                        "project-images/default.png")
+                                                            .getKey();
+            assertEquals(expected, actual);
+
+        } catch (AmazonS3Exception e) {
             System.out.println(e.getMessage());
             fail();
         }
@@ -40,7 +53,31 @@ class S3BucketServiceIntegrationTest {
 
     @Test
     public void listFiles_successfulScenario() {
-        s3BucketService.listFiles("jonij-portfolio-backend");
+        s3BucketService.listFiles();
+    }
+
+    @Test
+    public void getProjectImage_successfulScenario() {
+        try {
+            Resource actual = s3BucketService.getProjectImage("default.png");
+
+            assertNotNull(actual);
+
+        } catch (AmazonS3Exception e) {
+            System.out.println(e.getMessage());
+            fail();
+        }
+    }
+
+    @Test
+    public void getProjectImage_negativeScenario() {
+        try {
+            Resource actual = s3BucketService.getProjectImage("car.png");
+            fail();
+
+        } catch (AmazonS3Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
