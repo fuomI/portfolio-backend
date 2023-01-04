0d95f4e2a4a1fe7bfd457ce0f43d98437bca2355
Joni Jansson <joni.jansson@saunalahti.fi>
2023-01-04 10:01:24 +0200

    Coding task 12: PortfolioBackendApplication: Removed @Configuration related to old Storage classes

--- src/main/java/fi/jonij/portfoliobackend/PortfolioBackendApplication.java
+++ src/main/java/fi/jonij/portfoliobackend/PortfolioBackendApplication.java
@@ -1,16 +1,13 @@
 package fi.jonij.portfoliobackend;
 
-import fi.jonij.portfoliobackend.storage.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 // Commented code needed when deployed in web container
 
 @SpringBootApplication
-@EnableConfigurationProperties(StorageProperties.class)
 public class PortfolioBackendApplication extends SpringBootServletInitializer {
 
 	@Override
